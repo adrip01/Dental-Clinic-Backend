1167ebab4da07c8e26bcb0c2483754f1938b2179
Adriana <adrianapardo1998@gmail.com>
2023-06-28 09:12:36 +0200

    update getall

--- controllers/user/getAll.js
+++ controllers/user/getAll.js
@@ -6,31 +6,31 @@ const models = require("../../models/index");
 console.log(models.sequelize.models);
 
 module.exports = async (req, res) => {
-  //   let { page } = req.query;
+  let { page } = req.query;
 
-  //   page = +page;
-  //   if (!page || page < 0) page = 1;
+  page = +page;
+  if (!page || page < 0) page = 1;
 
-  //   const LIMIT = 6;
-  //   const userCount = await User.count();
-  //   const maxPages = Math.ceil(userCount / LIMIT);
+  const LIMIT = 6;
+  const userCount = await User.count();
+  const maxPages = Math.ceil(userCount / LIMIT);
 
-  //   if (page > maxPages) {
-  //     return res.status(404).json({
-  //       status: "Error",
-  //       message: errorMsg.pagination.SURPASS,
-  //     });
-  //   }
+  if (page > maxPages) {
+    return res.status(404).json({
+      status: "Error",
+      message: errorMsg.pagination.SURPASS,
+    });
+  }
 
   try {
     const users = await User.findAll({
-      //   limit: LIMIT,
-      //   offset: (page - 1) * LIMIT,
-      //   attributes: [
-      //     "id",
-      //     ["user_first_name", "name"],
-      //     ["user_last_name", "last_name"],
-      //   ],
+      limit: LIMIT,
+      offset: (page - 1) * LIMIT,
+      attributes: [
+        "id",
+        ["user_first_name", "name"],
+        ["user_last_name", "last_name"],
+      ],
       attributes: { exclude: ["password", "createdAt", "updatedAt"] },
 
       include: [
@@ -39,19 +39,19 @@ module.exports = async (req, res) => {
           as: "role",
           attributes: { exclude: ["createdAt", "updatedAt"] },
         },
-        // {
-        //   model: Customer,
-        //   as: "customer",
-        //   attributes: { exclude: ["createdAt", "updatedAt"] },
-        // },
+        {
+          model: Customer,
+          as: "customer",
+          attributes: { exclude: ["createdAt", "updatedAt"] },
+        },
       ],
     });
     res.status(200).json({
-      //   info: {
-      //     count: userCount,
-      //     page,
-      //     pages: maxPages,
-      //   },
+      info: {
+        count: userCount,
+        page,
+        pages: maxPages,
+      },
       results: users,
     });
   } catch (error) {
